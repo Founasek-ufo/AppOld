9203a60bd06396885caa986d7c5864fba9f8967b
Marek Vondra <maravondra@gmail.com>
2019-07-11 00:46:23 +0200

    in main screen add the temperature indicator

--- app/src/main/java/com/example/homesensors/MainActivity.kt
+++ app/src/main/java/com/example/homesensors/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.homesensors
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
 
+    lateinit var currentTemperature: TextView
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        currentTemperature = findViewById(R.id.sensor_temperature)
+
+        currentTemperature.text = "10"
+//        currentTemperature.setText("10");
     }
+
+    override fun onClick(v: View?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 }
